be9eb13b9690ab701ddd24483d514997cf4e07d7
waitin <waitinjittiruengwichai@gmail.com>
2025-05-28 09:33:42 +0700

    signinpagefix

--- src/app/api/auth/signin/page.tsx
+++ src/app/api/auth/signin/page.tsx
@@ -77,7 +77,7 @@ export default function SignInPage() {
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
-
+          <div className="bg-white p-8 rounded-2xl  w-full max-w-md max-h-[400px]"></div>
           <button
             type="submit"
             className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-200"
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import NavBar from "@/components/Navbar";
 import { getServerSession } from 'next-auth';
 import { authOptions } from './api/auth/[...nextauth]/authOptions';
 import NextAuthProvider from '@/providers/NextAuthProvider';
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,12 +24,10 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-
         <NextAuthProvider session={ nextAuthSession }>
-
+          <NavBar/>
           {children}
         </NextAuthProvider>
-
       </body>
     </html>
   );
